06882b8f3a699a14967a17a84b1cb4b303d1e5df
rishiraj-yuvaraj <rraj39667@gmail.com>
2023-01-27 21:18:26 +0530

    added render api

--- src/globalApi.js
+++ src/globalApi.js
@@ -1,4 +1,6 @@
 
 //export const API = "https://636e65a3182793016f3fb576.mockapi.io";
 
-export const API = "http://localhost:4000/";
+//export const API = "http://localhost:4000";
+
+export const API = "https://mongo-node-rishiraj.onrender.com";
